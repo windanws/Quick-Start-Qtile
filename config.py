c4e2fe0cce8d0cae9e71905f29ea8c195ad39b22
windanws <winstondang00@gmail.com>
2023-06-30 15:53:21 +1000

    Add new Config File

--- config.py
+++ config.py
@@ -1,39 +1,37 @@
-#  --- Whisk Qtile Config File ---
+# lang: UTF-8
 #
+# ███╗░░░███╗██╗██╗░░░██╗██╗░░░██╗
+# ████╗░████║██║╚██╗░██╔╝██║░░░██║
+# ██╔████╔██║██║░╚████╔╝░██║░░░██║
+# ██║╚██╔╝██║██║░░╚██╔╝░░██║░░░██║
+# ██║░╚═╝░██║██║░░░██║░░░╚██████╔╝
+# ╚═╝░░░░░╚═╝╚═╝░░░╚═╝░░░░╚═════╝░
 #
-#  _______                                __                     
-# /       \                              /  |                    
-# $$$$$$$  | ______    ______    ______  $$/  _______    ______  
-# $$ |__$$ |/      \  /      \  /      \ /  |/       \  /      \ 
-# $$    $$//$$$$$$  |/$$$$$$  |/$$$$$$  |$$ |$$$$$$$  |/$$$$$$  |
-# $$$$$$$/ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |
-# $$ |     $$ \__$$ |$$ |__$$ |$$ |__$$ |$$ |$$ |  $$ |$$ \__$$ |
-# $$ |     $$    $$/ $$    $$/ $$    $$/ $$ |$$ |  $$ |$$    $$ |
-# $$/       $$$$$$/  $$$$$$$/  $$$$$$$/  $$/ $$/   $$/  $$$$$$$ |
-#                   $$ |      $$ |                    /  \__$$ |
-#                   $$ |      $$ |                    $$    $$/ 
-#                   $$/       $$/                      $$$$$$/  
-#
-#
-#
+# ~Qtile Config~
 
 import os
+import re
+import socket
 import subprocess
-from libqtile import bar, layout, widget, hook
+from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from typing import List
+
+from qtile_extras import widget
+# from qtile_extras.widget.decorations import PowerLineDecoration
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "alacritty"
 
 keys = [
-    # Switch between windows
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"), # Move windows between left/right columns or move up/down in current stack.
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
@@ -46,10 +44,7 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
+
     Key(
         [mod, "shift"],
         "Return",
@@ -61,90 +56,186 @@ keys = [
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod,], "l", lazy.spawn("i3lock"), desc="Screenlock Qtile"),
+    Key([mod], "r", lazy.spawn("rofi -combi-modi window, drun, ssh -font 'Jetbrains Mono 12' -show drun")),
+
+    # Control Volume
+    Key([], "XF86AudioMute", lazy.spawn("amixer -q sset Master toggle")),
+    Key([], "XF86AudioLowerVolume", lazy.widget["volume"].decrease_vol()),
+    Key([], "XF86AudioRaiseVolume", lazy.widget["volume"].increase_vol()),
+
+    # Brightness Controls
+    Key([], "XF86MonBrightnessDown", lazy.spawn("light -U 10")),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("light -A 10")),
+
+
 ]
 
 groups = [
-    Group('1', label="一"),
-    Group('2', label="二"),
-    Group('3', label="三"),
-    Group('4', label="四"),
-    Group('5', label="五"),
-    Group('6', label="六"),
-    Group('7', label="七"),
-    Group('8', label="八"),
-    Group('9', label="九"),
-]
+    Group('1',label='Dev'),
+    Group('2',label='Web'),
+    Group('3',label='Term'),
+    Group('4',label='File'),
+    Group('5',label='Disc'),
+    Group('6',label='Note'),
+    Group('7',label='PDF'),
+    Group('8',label='Music'),
+    Group('9',label='Doc'),
+    ]
+
 
 for i in groups:
     keys.extend(
         [
-            # mod1 + letter of group = switch to group
-            Key([mod], i.name, lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),),
-
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
             # mod1 + shift + letter of group = switch to & move focused window to group
-            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-                desc="Move focused window to group {}".format(i.name),),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
         ]
     )
 
+
+layout_theme = {"border_width": 2,
+                "margin": 8,
+                "border_focus": "#FFD95A",
+                "border_normal": "#4C3D3D"
+                }
+
 layouts = [
-    layout.Columns(border_focus="#BDAE93", border_normal="#282828",margin=8, border_width=2),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(margin=8),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.MonadTall(**layout_theme),
+    layout.Max(**layout_theme),
+    layout.Floating(**layout_theme),
 ]
 
+
+colors = [["#282c34", "#282c34"], # [0]
+          ["#1c1f24", "#1c1f24"], # [1]
+          ["#dfdfdf", "#dfdfdf"], # [2]
+          ["#FFF7D4", "#FFF7D4"], # [3]
+          ["#ff6c6b", "#ff6c6b"], # [4] Error Colour
+          ["#98be65", "#98be65"], # [5] Correct Colour
+          ["#FFD953", "#FFD953"], # [6] Main Colour
+          ["#4C3D3D", "#4C3D3D"], # [7] Secondary Colour
+          ["#A75D5D", "#A75D5D"], # [8]
+          ["#D3756B", "#D3756B"]] # [9]
+          
+
 widget_defaults = dict(
-    font="sans",
+    font="JetBrains Mono Bold",
     fontsize=12,
     padding=3,
 )
+
 extension_defaults = widget_defaults.copy()
+
+'''
+Arrow Directions
+powerline = {
+    "decorations": [
+            PowerLineDecoration()   
+        ]
+     }
+'''
+
 
 screens = [
     Screen(
-	wallpaper='~/.config/qtile/background.png',
-        wallpaper_mode='stretch',
+        wallpaper='~/Picture/ubuntu.jpg',
         top=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
+                widget.CurrentLayoutIcon(
+                    scale = 0.5,
+                    padding = 0,
+                    ),
+                widget.GroupBox(
+                    active = colors[2],
+                    inactive = colors[0],
+                    highlight_color = colors[7],
+                    highlight_method = "line",
+                    this_current_screen_border = colors[6],
+                    foreground = colors[2],
+                    ),
                 widget.Prompt(),
-                widget.WindowName(),
+                widget.WindowName(
+                    font = "JetBrains Mono",
+                    fontsize = 11,
+                    ),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
+                widget.Sep(
+                    linewidth=1,
+                    padding=10,
+                    ),
+                widget.Volume(
+                    fmt = "  {}"
+                    ),
+                widget.Sep(
+                    linewidth=1,
+                    padding=10,
+                    ),
+                widget.Backlight(
+                    fmt = " {}", 
+                    backlight_name ="amdgpu_bl0",
+                    update_interval = 0.3,
+                    ),
+                widget.TextBox(
+                    font="FontAwesome",
+                    text="   ",
+                    padding = 0, 
+                    mouse_callbacks = {'Button1': lazy.spawn('alacritty --hold -e acpi')},
+                    ),
+                widget.Battery(
+                    format = "{percent:2.0%}",
+                    low_percentage = 0.1,
+                    update_interval = 60,
+                    mouse_callbacks = {"Button1": lazy.spawn('alacritty --hold -e acpi')},
+                    **widget_defaults,
+                    ),
+                widget.Sep(
+                    linewidth=1,
+                    padding=10,
+                    ),
+                widget.Systray(padding = 1),
+                widget.Sep(
+                    linewidth=1,
+                    padding=10,
+                    ),
+
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Sep(
+                    linewidth=1,
+                    padding=10,
+                        ),
+                widget.Wlan(
+                    format = "{essid}",
+                    mouse_callbacks = {"Button1": lazy.spawn("alacritty --hold -e cat .config/qtile/Guides/networkGuide.md")},
+                        ),
+                widget.Sep(
+                    linewidth = 0, 
+                    padding = 6,
+                    foreground=colors[1],
+
+                        ),
             ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            32,
+            background = colors[1],
         ),
     ),
 ]
+
 
 # Drag floating layouts.
 mouse = [
@@ -178,20 +269,8 @@ reconfigure_screens = True
 # focus, should we respect this or not?
 auto_minimize = True
 
+
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
-
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser('~')
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
